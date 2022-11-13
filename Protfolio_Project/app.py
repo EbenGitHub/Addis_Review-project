d6abd1a097ee083370bca9e4fa86440017627434
EbenGitHub <abenezergoo@gmail.com>
2022-11-13 04:23:35 +0300

    rate and review

--- Protfolio_Project/app.py
+++ Protfolio_Project/app.py
@@ -2,8 +2,8 @@ from flask import Flask, render_template, redirect
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
+@app.route('/restaurants')
+def restaurants():
     return render_template("Restaurant_page_test.html")
 
 @app.route('/login')
@@ -13,6 +13,14 @@ def login():
 @app.route('/signup')
 def signup():
     return render_template("signup.html")
+
+@app.route('/foods')
+def foods():
+    return render_template("Food_Page_test.html")
+
+@app.route('/reviews')
+def reviews():
+    return render_template("review.html")
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
