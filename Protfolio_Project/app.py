202ec7eb7be76990edf47db524cd20a162b0696b
EbenGitHub <abenezergoo@gmail.com>
2022-11-13 16:31:29 +0300

    landing html

--- Protfolio_Project/app.py
+++ Protfolio_Project/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/restaurants')
 def restaurants():
-    return render_template("Restaurant_page_test.html")
+    return render_template("restaurant.html")
 
 @app.route('/login')
 def login():
@@ -14,13 +14,21 @@ def login():
 def signup():
     return render_template("signup.html")
 
+@app.route('/')
+def index():
+    return render_template("landing.html")
+
 @app.route('/foods')
 def foods():
-    return render_template("Food_Page_test.html")
+    return render_template("food.html")
 
 @app.route('/reviews')
 def reviews():
     return render_template("review.html")
+
+@app.route('/register', methods=["POST", "PUT"])
+def register():
+    
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
